ebf4b5f619218e5d721a17a1a17ea189bfc82736
Dariusz Okoński <darek200180@gmail.com>
2023-05-25 18:43:55 +0200

    day 6

--- src/week_01/day5/exercises/Main.java
+++ src/week_01/day5/exercises/Main.java
@@ -3,5 +3,6 @@ package week_01.day5.exercises;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
     }
 }
---
+++ src/week_02/day6/Main.java
@@ -0,0 +1,7 @@
+package week_02.day6;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Day 6");
+    }
+}
