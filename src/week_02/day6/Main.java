449ac4adc05ad5911ee67a930fe1830923d863a4
Dariusz Okoński <darek200180@gmail.com>
2023-05-25 19:11:25 +0200

    day 6

--- src/week_02/day6/Main.java
+++ src/week_02/day6/Main.java
@@ -1,7 +1,30 @@
 package week_02.day6;
 
+import week_02.day6.animal.Cat;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Day 6");
+        Cat cat1 = new Cat("Rudy");
+        Cat cat2 = new Cat("Klakier");
+
+        System.out.println(cat1.leftLegBack);
+        System.out.println(cat1.gimmeVoice());
+        System.out.println(cat1.getName());
+        System.out.println(cat2.getName());
+
+        System.out.println("==================");
+        Cat cat3 = new Cat("mruczek");
+        System.out.println(cat3.getName());
+        cat3.setName("spioch");
+        System.out.println(cat3.getName());
+
+        newMethod();
+    }
+
+    private static void newMethod() {
+        String variable = "variable";
+        System.out.println(variable);
     }
 }
+
+
---
+++ src/week_02/day6/animal/Dog.java
@@ -0,0 +1,12 @@
+package week_02.day6.animal;
+
+public class Dog {
+    public String leftLegFront = "LLF";
+    public String rightLegFront = "RLF";
+    public String leftLegBack = "LLB";
+    public String rightLegBack = "RLB";
+
+    public String gimmeVoice() {
+        return  "woof!!!";
+    }
+}
