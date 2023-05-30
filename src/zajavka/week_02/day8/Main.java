20ae86fb306ff13d04c7f67d5e238d293962c4c5
Dariusz Okoński <darek200180@gmail.com>
2023-05-30 20:32:20 +0200

    add day 8

---
+++ src/zajavka/week_02/day8/Main.java
@@ -0,0 +1,10 @@
+package zajavka.week_02.day8;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Hello world1";
+
+        StringBuilder sb = new StringBuilder();
+
+    }
+}
