c98353000bff2270cd961e6dcb3b9eede5b71be6
Dariusz Okoński <darek200180@gmail.com>
2023-05-30 19:58:33 +0200

    add day7ext

---
+++ src/zajavka/week_02/day7ext/Cat.java
@@ -0,0 +1,5 @@
+package zajavka.week_02.day7ext;
+
+public class Cat {
+    public String name = "Mruczek";
+}
--- src/zajavka/week_02/day7ext/Main.java
+++ src/zajavka/week_02/day7ext/Main.java
@@ -2,6 +2,15 @@ package zajavka.week_02.day7ext;
 
 public class Main {
     public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat cat2 = cat1;
+
+        System.out.println(cat1.name);
+        System.out.println(cat2.name);
+        cat1.name = "Burek";
+        System.out.println(cat1.name);
+        System.out.println(cat2.name);
 
     }
 }
+
