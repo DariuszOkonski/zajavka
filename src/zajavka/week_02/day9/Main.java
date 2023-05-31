7c2fb0043c43e2a5b47f5adc8e4f298a78020f64
Dariusz Okoński <darek200180@gmail.com>
2023-05-31 17:41:29 +0200

    day 9

--- src/zajavka/week_02/day9/Main.java
+++ src/zajavka/week_02/day9/Main.java
@@ -2,6 +2,6 @@ package zajavka.week_02.day9;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("day o");
+        System.out.println("day 9");
     }
 }
