cd5eec763cfdb8860d4f36979c77c4ee87be27a8
Dariusz Okoński <darek200180@gmail.com>
2023-05-31 20:02:22 +0200

    day 9

--- src/zajavka/week_02/day9/Main.java
+++ src/zajavka/week_02/day9/Main.java
@@ -2,6 +2,11 @@ package zajavka.week_02.day9;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("day 9");
+        int a = 2;
+        int b = 3;
+        String c = "String";
+
+        String anotherValue = String.format("some value is: %s another value is: %s and again value is: %s",a,b,c);
+        System.out.println(anotherValue);
     }
 }
