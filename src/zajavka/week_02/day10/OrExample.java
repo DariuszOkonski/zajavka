2d74964e9e52c18ad2598facf3ed164b492a3504
Dariusz Okoński <darek200180@gmail.com>
2023-06-01 19:00:52 +0200

    day 10

--- src/zajavka/week_02/day10/OrExample.java
+++ src/zajavka/week_02/day10/OrExample.java
@@ -2,6 +2,24 @@ package zajavka.week_02.day10;
 
 public class OrExample {
     public static void main(String[] args) {
+        /*
+        * 4 => 0 1 0 0
+        * 5 => 0 1 0 1
+
+        * & => 0 1 0 0  4
+        * | => 0 1 0 1  5
+        * ^ => 0 0 0 1  1
+        * */
+
+        /*
+         * 12 => 0 1 1 0 0
+         * 16 => 1 0 0 0 0
+
+         * & =>  0 0 0 0 0  0
+         * | =>  1 1 1 0 0  28
+         * ^ =>  1 1 1 0 0  28
+         * ~ =>  1 0 0 1 1  17
+         * */
 
     }
 }
