5f2b1ecfda543a1304bda8d71be19e860f6a9f9f
Dariusz Okoński <darek200180@gmail.com>
2023-06-01 17:07:00 +0200

    day 9

--- src/zajavka/week_02/day9ext/Main.java
+++ src/zajavka/week_02/day9ext/Main.java
@@ -2,6 +2,11 @@ package zajavka.week_02.day9ext;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("day 9");
+        String a = "wow";
+        String b = new String("wow");
+        b = b.intern();
+
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
     }
 }
