c51857096d6d1fe6b4e15bb81f149e19c0aff7f0
Dariusz Okonski <darek200180@gmail.com>
2023-06-05 16:25:55 +0200

    add externalMethod

--- src/zajavka/week_02/day10ext/Main.java
+++ src/zajavka/week_02/day10ext/Main.java
@@ -2,6 +2,33 @@ package zajavka.week_02.day10ext;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        externalMethod(method1() + method2(method3() + method4()));
+
+
+    }
+
+    public static void externalMethod(int arg) {
+        System.out.println("externalMethod");
+        System.out.println("Result: " + arg);
+    }
+
+    private static int method1() {
+        System.out.println("method1");
+        return 2;
+    }
+
+    private static int method2(int i) {
+        System.out.println("method2");
+        return i;
+    }
+
+    private static int method3() {
+        System.out.println("method3");
+        return 3;
+    }
+
+    private static int method4() {
+        System.out.println("method4");
+        return 3;
     }
 }
