853663e96b3636b0f9c3ee8846e9ac68ae00858e
Dariusz Okoński <darek200180@gmail.com>
2023-06-06 17:49:17 +0200

    ternary operator

--- src/zajavka/week_03/day11ext/App.java
+++ src/zajavka/week_03/day11ext/App.java
@@ -2,6 +2,21 @@ package zajavka.week_03.day11ext;
 
 public class App {
     public static void main(String[] args) {
+       int a = 3;
 
+       String result = (a == 5) ? "piec" : "niepiec";
+       String result2 = (a < 4) ? stringMethod1() : stringMethod2();
+
+        System.out.println(result);
+        System.out.println(result2);
+
+    }
+
+    public static String stringMethod1() {
+        return "piec";
+    }
+
+    public static String stringMethod2() {
+        return "niepiec";
     }
 }
