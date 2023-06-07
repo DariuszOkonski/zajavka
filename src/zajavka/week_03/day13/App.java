c9c02e1309e1ad4bfd22179a40a00c73ddbf9b1a
Dariusz Okoński <darek200180@gmail.com>
2023-06-07 17:49:14 +0200

    day 13

--- src/zajavka/week_03/day13/App.java
+++ src/zajavka/week_03/day13/App.java
@@ -1,7 +1,29 @@
 package zajavka.week_03.day13;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        arraysExample();
+    }
+
+    private static void arraysExample() {
+        double[] array = new double[] {1.0, 2.0, 3.72, 3.0, 9.34, 1.29, 2.55, 1.23894};
+
+        // total
+        double total = 0;
+        for (double element: array) {
+            total += element;
+        }
+        System.out.println("Total: " + total);
+
+        // max
+        double maximum = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] > maximum) {
+                maximum = array[i];
+            }
+        }
+        System.out.println("Max: " + maximum);
     }
 }
