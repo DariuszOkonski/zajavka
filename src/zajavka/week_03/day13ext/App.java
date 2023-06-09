2bd032266979c979ff8a1a18663fe66e5674be1b
Dariusz Okoński <darek200180@gmail.com>
2023-06-09 16:48:09 +0200

    day 13

--- src/zajavka/week_03/day13ext/App.java
+++ src/zajavka/week_03/day13ext/App.java
@@ -1,7 +1,28 @@
 package zajavka.week_03.day13ext;
 
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int[] array = {7, 9, 11, -1};
+
+        for (int element: array) {
+            System.out.println(element);
+        }
+    }
+
+    private static void printArrayElements(int[] input) {
+        System.out.print('[');
+//        for (int i = 0; i < input.length; i++) {
+//            System.out.print(input[i] + ((i != input.length - 1) ? ", " : ""));
+//        }
+
+        int i = 0;
+        for (int element: input) {
+            System.out.print(element + ((i != input.length - 1) ? ", " : ""));
+            i++;
+        }
+
+        System.out.print(']');
     }
 }
