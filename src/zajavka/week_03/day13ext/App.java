777fbc48a52e8c60686cc2f645a8fd6b3a17414a
Dariusz Okoński <darek200180@gmail.com>
2023-06-09 17:11:13 +0200

    day 13

--- src/zajavka/week_03/day13ext/App.java
+++ src/zajavka/week_03/day13ext/App.java
@@ -4,25 +4,19 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        int[] array = {7, 9, 11, -1};
+        int[] array = new int[]{7, 9, 11, -1, 15};
 
+        System.out.println("avg = " + getAverageValue(array));
+    }
+
+    private static double getAverageValue(int[] array) {
+        int sum = 0;
         for (int element: array) {
-            System.out.println(element);
-        }
-    }
-
-    private static void printArrayElements(int[] input) {
-        System.out.print('[');
-//        for (int i = 0; i < input.length; i++) {
-//            System.out.print(input[i] + ((i != input.length - 1) ? ", " : ""));
-//        }
-
-        int i = 0;
-        for (int element: input) {
-            System.out.print(element + ((i != input.length - 1) ? ", " : ""));
-            i++;
+            sum += element;
         }
 
-        System.out.print(']');
+        System.out.println("sum = " + sum);
+        return ((double)sum / array.length);
     }
+
 }
