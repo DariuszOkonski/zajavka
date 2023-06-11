a6d5c6453c7b0bc148d06b48e86ca0946f305721
Dariusz Okoński <darek200180@gmail.com>
2023-06-11 10:01:43 +0200

    day 16 ext

--- src/zajavka/week_04/day16ext/App.java
+++ src/zajavka/week_04/day16ext/App.java
@@ -2,6 +2,8 @@ package zajavka.week_04.day16ext;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("day 16 ext");
+        Tuna tuna1 = new Tuna();
+        tuna1.fly();
     }
 }
+
---
+++ src/zajavka/week_04/day16ext/Tuna.java
@@ -0,0 +1,17 @@
+package zajavka.week_04.day16ext;
+
+public class Tuna {
+    public String name;
+    public int age;
+    public Integer length;
+
+    public void fly() {
+        String color = "pink";
+
+        System.out.println("Color: " + color);
+        System.out.println("Swimming: " + name);
+        System.out.println("age: " + age);
+        System.out.println("length: " + length);
+    }
+
+}
