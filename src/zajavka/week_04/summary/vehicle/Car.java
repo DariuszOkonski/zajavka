1b6d6f9b02767f1d154dd80d40ed4cf726c22441
Dariusz Okoński <darek200180@gmail.com>
2023-06-19 18:19:29 +0200

    summary

--- src/zajavka/week_04/summary/vehicle/Car.java
+++ src/zajavka/week_04/summary/vehicle/Car.java
@@ -1,5 +1,7 @@
 package zajavka.week_04.summary.vehicle;
 
+import java.util.Objects;
+
 public class Car {
     String brand;
     String model;
@@ -11,6 +13,24 @@ public class Car {
 
     void drive() {
         System.out.println("driving: " + this.brand + " " + this.model);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (!Objects.equals(brand, car.brand)) return false;
+        return Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = brand != null ? brand.hashCode() : 0;
+        result = 31 * result + (model != null ? model.hashCode() : 0);
+        return result;
     }
 
     @Override
