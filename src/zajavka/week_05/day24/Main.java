5f0ec35b2d31d3cc6f43332843a1912ca905e0a2
Dariusz Okoński <darek200180@gmail.com>
2023-06-22 18:03:52 +0200

    day 24

--- src/zajavka/week_05/day24/Main.java
+++ src/zajavka/week_05/day24/Main.java
@@ -1,7 +1,12 @@
 package zajavka.week_05.day24;
 
+import zajavka.week_02.summary.fajna.Car;
+
 public class Main {
     public static void main(String[] args) {
-        // day 24
+        Car car1 = new Car();
+        var car2 = new zajavka.week_04.summary.vehicle.Car("bmw", "5");
+        var car3 = new zajavka.week_05.day21.gettersSetters.Car("bmw", "3", 2020);
+
     }
 }
