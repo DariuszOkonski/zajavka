b759fc59313ecf6f958f91f3f9b16c40c03dfd72
Dariusz Okoński <darek200180@gmail.com>
2023-06-28 19:26:13 +0200

    day 27

--- src/zajavka/week_06/day27/App.java
+++ src/zajavka/week_06/day27/App.java
@@ -2,11 +2,11 @@ package zajavka.week_06.day27;
 
 public class App {
     public static void main(String[] args) {
-        Cabriolet cabriolet = new Cabriolet("BMW", "8", "red", false);
+        Cabriolet cabriolet = new Cabriolet("BMW", "8", "red", false,
+                "fire extinguisher", "message");
         cabriolet.greetings();
         cabriolet.closeRoof();
         cabriolet.countAdditionalEquipmentElements();
         cabriolet.getAdditionalEquipment();
-
     }
 }
