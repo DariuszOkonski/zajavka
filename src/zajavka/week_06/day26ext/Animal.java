5e306c885eecc0f3c4748facae18154f9525b054
Dariusz Okoński <darek200180@gmail.com>
2023-06-28 18:07:24 +0200

    day26

---
+++ src/zajavka/week_06/day26ext/Animal.java
@@ -0,0 +1,9 @@
+package zajavka.week_06.day26ext;
+
+public enum Animal {
+    REINDEER,
+    SHARK,
+    CROCODILE,
+    FROG,
+    GIRAFFE;
+}
---
+++ src/zajavka/week_06/day26ext/App.java
@@ -0,0 +1,7 @@
+package zajavka.week_06.day26ext;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
