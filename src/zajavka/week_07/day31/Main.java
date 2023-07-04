7d642bf60fd1a8033a95337389abd06ae306dd01
Dariusz Okoński <darek200180@gmail.com>
2023-07-04 18:29:26 +0200

    exceptions

--- src/zajavka/week_07/day31/Main.java
+++ src/zajavka/week_07/day31/Main.java
@@ -3,21 +3,12 @@ package zajavka.week_07.day31;
 public class Main {
     public static void main(String[] args) {
 
-        try {
-            printSomething("Throw");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            printSomething("Throws");
-        } catch (MyException e) {
-            e.printStackTrace();
-        }
+        printSomething("Throws");
+        printSomething("Throw");
 
     }
 
-    private static void printSomething(final String input) throws MyException {
+    private static void printSomething(final String input) {
         if("Throw".equalsIgnoreCase((input))) {
             throw new MyException();
         } else {
--- src/zajavka/week_07/day31/MyException.java
+++ src/zajavka/week_07/day31/MyException.java
@@ -1,6 +1,6 @@
 package zajavka.week_07.day31;
 
-public class MyException extends Exception {
+public class MyException extends RuntimeException {
     public MyException() {
         super("My exception was thrown");
     }
