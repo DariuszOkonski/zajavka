f127893297d57ed351f7f608de00af45909687ff
Dariusz Okoński <darek200180@gmail.com>
2023-07-06 19:05:46 +0200

    day 35

---
+++ src/zajavka/week_07/day35/Main.java
@@ -0,0 +1,10 @@
+package zajavka.week_07.day35;
+
+import java.util.Hashtable;
+
+public class Main {
+    public static void main(String[] args) {
+        Hashtable hashtable = new Hashtable();
+        hashtable.put(null, 123);
+    }
+}
