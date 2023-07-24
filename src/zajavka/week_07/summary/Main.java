2a0bec376d7ba243e64331401a3bd1800e1db085
Dariusz Okoński <darek200180@gmail.com>
2023-07-24 10:58:07 +0200

    exceptions

--- src/zajavka/week_07/summary/Main.java
+++ src/zajavka/week_07/summary/Main.java
@@ -2,6 +2,18 @@ package zajavka.week_07.summary;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("summary");
+        System.out.println("Printing Before");
+
+        try {
+            someOtherMethod();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        System.out.println("Printing After");
+    }
+
+    private static void someOtherMethod() throws RuntimeException {
+        throw new RuntimeException("Some error");
     }
 }
