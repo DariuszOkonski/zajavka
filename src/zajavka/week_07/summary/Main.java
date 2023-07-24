bbc7395764fa236757c167a557175b5c5916f028
Dariusz Okoński <darek200180@gmail.com>
2023-07-24 10:30:42 +0200

    summary

--- src/zajavka/week_07/summary/Main.java
+++ src/zajavka/week_07/summary/Main.java
@@ -1,7 +1,7 @@
-package zajavka.week_07.day35ext;
+package zajavka.week_07.summary;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("summary");
     }
 }
