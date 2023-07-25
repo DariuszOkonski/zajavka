22b2a78b04e6bda3d4b1275c324b2015102201e5
Dariusz Okoński <darek200180@gmail.com>
2023-07-25 12:12:50 +0200

    day 40

--- src/zajavka/week_08/day40/Main.java
+++ src/zajavka/week_08/day40/Main.java
@@ -1,7 +1,25 @@
 package zajavka.week_08.day40;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> listWitchCities = new ArrayList<>();
+        listWitchCities.add("Warsaw");
+        listWitchCities.add("Gdansk");
+        listWitchCities.add("Lodz");
+        listWitchCities.add("Wroclaw");
 
+        ListIterator<String> stringListIterator = listWitchCities.listIterator();
+        while (stringListIterator.hasNext()) {
+            System.out.println(stringListIterator.next());
+        }
+        System.out.println("===");
+        while (stringListIterator.hasPrevious()) {
+            System.out.println(stringListIterator.previous());
+        }
     }
 }
