734b26477922a32f1c19744d26c2e0225b78203d
Dariusz Okoński <darek200180@gmail.com>
2023-07-25 14:48:35 +0200

    day41ext

---
+++ src/zajavka/week_09/day41/Main.java
@@ -0,0 +1,11 @@
+package zajavka.week_09.day41;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+    public static void recursive() {
+        recursive();
+    }
+}
