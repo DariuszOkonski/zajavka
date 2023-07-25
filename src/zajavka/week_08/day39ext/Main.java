fd98d3796a02eee54d0b38c414dc6da122c7f572
Dariusz Okoński <darek200180@gmail.com>
2023-07-25 11:29:23 +0200

    day 39

--- src/zajavka/week_08/day39ext/Main.java
+++ src/zajavka/week_08/day39ext/Main.java
@@ -1,7 +1,21 @@
 package zajavka.week_08.day39ext;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("day 39");
+        Map<String, List<String>> citiesAttractions = new HashMap<>();
+
+        citiesAttractions.put("Warsaw", List.of("Palac Kultury i Nauki", "Metro Politechnika"));
+        citiesAttractions.put("Gdynia", Arrays.asList("Galeria Handlowa", "Neptun"));
+
+        List<String> londonsAttractions = new ArrayList<>();
+        londonsAttractions.add("Londons Eye");
+        londonsAttractions.add("Buckingham Palace");
+        citiesAttractions.put("London", londonsAttractions);
+
+        System.out.println(citiesAttractions);
+        Set<Map.Entry<String, List<String>>> entries = citiesAttractions.entrySet();
+        System.out.println(entries);
     }
 }
