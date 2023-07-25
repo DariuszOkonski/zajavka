b3d8dc1379f0609f1a83f19e7da67589e10b0cc0
Dariusz Okoński <darek200180@gmail.com>
2023-07-25 14:04:40 +0200

    summary

--- src/zajavka/week_08/summary/Main.java
+++ src/zajavka/week_08/summary/Main.java
@@ -1,7 +1,16 @@
 package zajavka.week_08.summary;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("summary");
+        List<String> someStringList = new ArrayList<>();
+        Set<String> someSet = new HashSet<>();
+        Map<String, String> someMap = new HashMap<>();
+
+
+        System.out.println(someStringList);
+        System.out.println(someSet);
+        System.out.println(someMap);
     }
 }
