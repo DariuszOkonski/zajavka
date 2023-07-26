bd9a2ea1aa0e6625e09dd9f3d3d1af19b7f463b6
Dariusz Okoński <darek200180@gmail.com>
2023-07-26 12:22:52 +0200

    day 42ext

--- src/zajavka/week_09/day42/Main.java
+++ src/zajavka/week_09/day42/Main.java
@@ -1,7 +1,12 @@
 package zajavka.week_09.day42;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("day 42");
+        System.out.println(LocalDate.ofYearDay(2023, 365));
     }
 }
---
+++ src/zajavka/week_09/day42ext/Main.java
@@ -0,0 +1,7 @@
+package zajavka.week_09.day42ext;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
