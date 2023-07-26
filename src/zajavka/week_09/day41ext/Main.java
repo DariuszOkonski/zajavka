4a4aaf74560b29b5d5a39b18709a13c8c542a78a
Dariusz Okoński <darek200180@gmail.com>
2023-07-26 11:27:54 +0200

    day 41

--- src/zajavka/week_09/day41ext/Main.java
+++ src/zajavka/week_09/day41ext/Main.java
@@ -3,5 +3,14 @@ package zajavka.week_09.day41ext;
 public class Main {
     public static void main(String[] args) {
         System.out.println("day 41ext");
+        recursiveMethod("Zajavk");
+    }
+
+    public static void recursiveMethod(String anything) {
+        if(anything.length() > 100) {
+            return;
+        }
+        System.out.println(anything);
+        recursiveMethod(anything + "a");
     }
 }
