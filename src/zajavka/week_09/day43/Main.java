9d7735c644623ae9d8ad6d80e118e92190c57a25
Dariusz Okoński <darek200180@gmail.com>
2023-07-26 13:30:25 +0200

    day 43

---
+++ src/zajavka/week_09/day43/Main.java
@@ -0,0 +1,9 @@
+package zajavka.week_09.day43;
+
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ZonedDateTime.now());
+    }
+}
