83d5edad6d3dd3f9cfdfca842b97df40b00649d8
Dariusz Okoński <darek200180@gmail.com>
2023-07-27 13:19:35 +0200

    day 44ext

---
+++ src/zajavka/week_09/day44ext/Main.java
@@ -0,0 +1,11 @@
+package zajavka.week_09.day44ext;
+
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime parse = LocalDateTime.parse("2020-10-10T10:10:10");
+
+        System.out.println(parse);
+    }
+}
