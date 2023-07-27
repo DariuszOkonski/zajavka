c9a3ff91002a892f4dbaac6686ce8360440a6026
Dariusz Okoński <darek200180@gmail.com>
2023-07-27 11:38:43 +0200

    day 43

--- src/zajavka/week_09/day43ext/Main.java
+++ src/zajavka/week_09/day43ext/Main.java
@@ -1,7 +1,18 @@
 package zajavka.week_09.day43ext;
 
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("day 43");
+        Period period = Period.of(0, 11, 22);
+
+        LocalDateTime localDateTime = LocalDateTime.of(2020,1,1,1,11);
+        LocalDateTime localDateTime2 = LocalDateTime.of(2021,3,4,1,11);
+
+        long between = ChronoUnit.DAYS.between(localDateTime, localDateTime2);
+        System.out.println(between);
+
+
     }
 }
