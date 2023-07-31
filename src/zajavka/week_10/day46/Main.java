3459049b5ce5cc1b6056152aaaac684fad443d62
Dariusz Okoński <darek200180@gmail.com>
2023-07-31 15:19:30 +0200

    day 46

--- src/zajavka/week_10/day46/Main.java
+++ src/zajavka/week_10/day46/Main.java
@@ -3,9 +3,6 @@ package zajavka.week_10.day46;
 public class Main {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder("Hello");
-
-        String hello = "Hello";
-        hello += " World!";
-        System.out.println(hello);
+        System.out.println(sb.capacity());
     }
 }
---
+++ src/zajavka/week_10/day46ext/Main.java
@@ -0,0 +1,12 @@
+package zajavka.week_10.day46ext;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 199; i++) {
+            stringBuilder.append("a");
+        }
+
+        System.out.println(stringBuilder);
+    }
+}
