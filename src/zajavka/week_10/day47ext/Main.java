ff6e79cadd8e0aafe8afc4616f598ad137d7be1c
Dariusz Okoński <darek200180@gmail.com>
2023-07-31 15:58:11 +0200

    day 47ext

--- src/zajavka/week_10/day47ext/Main.java
+++ src/zajavka/week_10/day47ext/Main.java
@@ -1,7 +1,15 @@
 package zajavka.week_10.day47ext;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        BigInteger bigInteger = new BigInteger("726347862347627482468264872462864382");
+        System.out.println(bigInteger);
+        System.out.println(bigInteger.intValue());
+
+        BigDecimal bigDecimal = BigDecimal.valueOf(123.001);
+
     }
 }
