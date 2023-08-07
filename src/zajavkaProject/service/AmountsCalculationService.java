f36ce2c6da8f5c5efdc7be3fc62b291124549eb4
Dariusz Okoński <darek200180@gmail.com>
2023-08-07 18:40:07 +0200

    add ratecalculation

--- src/zajavkaProject/service/AmountsCalculationService.java
+++ src/zajavkaProject/service/AmountsCalculationService.java
@@ -1,7 +1,11 @@
 package zajavkaProject.service;
 
+import zajavkaProject.model.InputData;
+import zajavkaProject.model.Rate;
 import zajavkaProject.model.RateAmounts;
 
 public interface AmountsCalculationService {
-    RateAmounts calculate();
+    RateAmounts calculate(InputData inputData);
+
+    RateAmounts calculate(InputData inputData, Rate previousRate);
 }
--- src/zajavkaProject/service/RateCalculationServiceImpl.java
+++ src/zajavkaProject/service/RateCalculationServiceImpl.java
@@ -49,14 +49,14 @@ public class RateCalculationServiceImpl implements RateCalculationService{
 
     private Rate calculateRate(BigDecimal rateNumber, InputData inputData) {
         TimePoint timePoint = timePointService.calculate(rateNumber, inputData);
-        RateAmounts rateAmounts = amountsCalculationService.calculate();
+        RateAmounts rateAmounts = amountsCalculationService.calculate(inputData);
         MortgageResidual mortgageResidual = residualCalculationService.calculate();
 
         return new Rate(rateNumber, timePoint, rateAmounts, mortgageResidual);
     }
     private Rate calculateRate(BigDecimal rateNumber, InputData inputData, Rate previousRate) {
         TimePoint timePoint = timePointService.calculate(rateNumber, inputData);
-        RateAmounts rateAmounts = amountsCalculationService.calculate();
+        RateAmounts rateAmounts = amountsCalculationService.calculate(inputData, previousRate);
         MortgageResidual mortgageResidual = residualCalculationService.calculate();
 
         return new Rate(rateNumber, timePoint, rateAmounts, mortgageResidual);
