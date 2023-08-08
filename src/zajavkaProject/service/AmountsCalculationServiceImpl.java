7729db21ddb3a714a5d1b5b02b0cd86bc4b364d1
Dariusz Okoński <darek200180@gmail.com>
2023-08-08 17:15:07 +0200

    some comment

--- src/zajavkaProject/service/AmountsCalculationServiceImpl.java
+++ src/zajavkaProject/service/AmountsCalculationServiceImpl.java
@@ -90,15 +90,15 @@ public class AmountsCalculationServiceImpl implements AmountsCalculationService{
         return amount
                 .multiply(q.pow(monthsDuration.intValue()))
                 .multiply(q.subtract(BigDecimal.ONE))
-                .divide(q.pow(monthsDuration.intValue()).subtract(BigDecimal.ONE), 10, RoundingMode.HALF_UP);
+                .divide(q.pow(monthsDuration.intValue()).subtract(BigDecimal.ONE), 50, RoundingMode.HALF_UP);
     }
     private BigDecimal calculateConstantCapitalAmount(BigDecimal rateAmount, BigDecimal interestAmount) {
         return rateAmount.subtract(interestAmount);
     }
     private BigDecimal calculateInterestAmount(BigDecimal residualAmount, BigDecimal interestPercent) {
-        return residualAmount.multiply(interestPercent).divide(YEAR, 10, RoundingMode.HALF_UP);
+        return residualAmount.multiply(interestPercent).divide(YEAR, 50, RoundingMode.HALF_UP);
     }
     private BigDecimal calculateDecreasingCapitalAmount(BigDecimal amount, BigDecimal monthsDuration) {
-        return amount.divide(monthsDuration, 10, RoundingMode.HALF_UP);
+        return amount.divide(monthsDuration, 50, RoundingMode.HALF_UP);
     }
 }
