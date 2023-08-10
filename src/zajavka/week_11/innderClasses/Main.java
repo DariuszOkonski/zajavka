179332eae4389c567f34c952b2bcea9a4cccf24b
Dariusz Okoński <darek200180@gmail.com>
2023-08-10 18:03:48 +0200

    last

--- src/zajavka/week_11/innderClasses/Main.java
+++ src/zajavka/week_11/innderClasses/Main.java
@@ -2,6 +2,6 @@ package zajavka.week_11.innderClasses;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("last");
     }
 }
