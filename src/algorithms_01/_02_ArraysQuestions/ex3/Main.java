5c0b380f798750f783faa4f4527e63e01851e982
Dariusz Okoński <darek200180@gmail.com>
2023-08-10 19:06:15 +0200

    add palindrome soution

---
+++ src/algorithms_01/_02_ArraysQuestions/ex3/Main.java
@@ -0,0 +1,10 @@
+package algorithms_01._02_ArraysQuestions.ex3;
+
+public class Main {
+    public static void main(String[] args) {
+        PalindromeProblem problem = new PalindromeProblem();
+        System.out.println(problem.solve("adam"));
+        System.out.println(problem.solve("madam"));
+    }
+}
+
