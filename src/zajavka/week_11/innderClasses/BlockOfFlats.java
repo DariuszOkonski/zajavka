00bf6c29c001305ef8bdd205b236af388a838845
Dariusz Okoński <darek200180@gmail.com>
2023-08-10 18:26:59 +0200

    innerclasses

--- src/zajavka/week_11/innderClasses/BlockOfFlats.java
+++ src/zajavka/week_11/innderClasses/BlockOfFlats.java
@@ -7,6 +7,9 @@ import java.util.List;
 public class BlockOfFlats {
     private List<Flat> flats;
 
+    public BlockOfFlats() {
+    }
+
     public BlockOfFlats(List<BigDecimal> areas) {
         List<Flat> flats = new ArrayList<>();
         for (BigDecimal area : areas) {
@@ -15,11 +18,26 @@ public class BlockOfFlats {
         this.flats = flats;
     }
 
-    private class Flat {
+    public List<Flat> getFlats() {
+        return flats;
+    }
+
+    public static class Flat {
         private BigDecimal area;
 
         public Flat(BigDecimal area) {
             this.area = area;
+        }
+
+        public BigDecimal getArea() {
+            return area;
+        }
+
+        @Override
+        public String toString() {
+            return "Flat{" +
+                    "area=" + area +
+                    '}';
         }
     }
 }
--- src/zajavka/week_11/innderClasses/Main.java
+++ src/zajavka/week_11/innderClasses/Main.java
@@ -1,8 +1,13 @@
 package zajavka.week_11.innderClasses;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        BlockOfFlats.Flat flat = new BlockOfFlats.Flat(BigDecimal.TEN);
 
+        System.out.println(flat);
     }
 }
 
