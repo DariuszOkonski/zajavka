0484c64a15f9e24214b070a27e7b678b87286396
Dariusz Okoński <darek200180@gmail.com>
2023-08-10 19:16:11 +0200

    integer reversion solution

---
+++ src/algorithms_01/_02_ArraysQuestions/ex4/Main.java
@@ -0,0 +1,9 @@
+package algorithms_01._02_ArraysQuestions.ex4;
+
+public class Main {
+    public static void main(String[] args) {
+        IntegerReversion problem = new IntegerReversion();
+        System.out.println(problem.reverse(1234));
+    }
+}
+
