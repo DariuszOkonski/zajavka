3cb9a2ff977b93ff7a335635ac338d81d66cbe7f
Dariusz Okoński <darek200180@gmail.com>
2023-08-17 18:39:10 +0200

    dates

--- src/java_A_Z/section07/ex3/Main.java
+++ src/java_A_Z/section07/ex3/Main.java
@@ -3,6 +3,7 @@ package java_A_Z.section07.ex3;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +11,14 @@ public class Main {
         System.out.println("Current Date: " + LocalDate.now());
         System.out.println("Current Time: " + LocalTime.now());
         System.out.println("Current Date & Time: " + LocalDateTime.now());
+
+        // Creating Dates and Times
+        LocalDate dateA = LocalDate.of(2020, Month.MARCH, 15);
+        LocalDate dateB = LocalDate.of(2020, 3, 15);
+        System.out.println("dateA: " + dateA + " dateB: " + dateB);
+
+        LocalTime time = LocalTime.of(9, 33);
+        System.out.println("time: " + time);
 
 
     }
