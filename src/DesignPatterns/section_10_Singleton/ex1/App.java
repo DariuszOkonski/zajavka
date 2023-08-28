0b5d0d703f9b453e69af9d48afffa0698d439a95
Dariusz Okoński <darek200180@gmail.com>
2023-08-28 17:48:20 +0200

    singleton

--- src/DesignPatterns/section_10_Singleton/ex1/App.java
+++ src/DesignPatterns/section_10_Singleton/ex1/App.java
@@ -2,6 +2,11 @@ package DesignPatterns.section_10_Singleton.ex1;
 
 public class App {
     public static void main(String[] args) {
+        DatabaseConnector object1 = DatabaseConnector.getInstance();
+        DatabaseConnector object2 = DatabaseConnector.getInstance();
 
+        System.out.println(object1 == object2);
+        System.out.println(object1.equals(object2));
     }
 }
+
