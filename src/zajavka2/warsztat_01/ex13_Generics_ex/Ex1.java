2451251bf0a3ca41730b715db67e5ab3713301f6
Dariusz Okoński <darek200180@gmail.com>
2023-09-04 17:20:07 +0200

    generics

---
+++ src/zajavka2/warsztat_01/ex13_Generics_ex/Ex1.java
@@ -0,0 +1,14 @@
+package zajavka2.warsztat_01.ex13_Generics_ex;
+
+import java.util.List;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static<T extends List<T>> int method(T[] arg1, T arg2) {
+
+        return 0;
+    }
+}
