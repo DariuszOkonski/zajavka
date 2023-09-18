fd9be9f96af2daa11aea9d4052dddf89138391eb
Dariusz Okonski <darek200180@gmail.com>
2023-09-18 19:07:24 +0200

    ex23

---
+++ src/zajavka2/warsztat_01/ex22/App.java
@@ -0,0 +1,10 @@
+package zajavka2.warsztat_01.ex22;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/zajavka2/warsztat_01/ex23/App.java
@@ -0,0 +1,7 @@
+package zajavka2.warsztat_01.ex23;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
