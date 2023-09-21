8c70b687dcd9d6407f47da56064af0da081a6683
Dariusz Okoński <darek200180@gmail.com>
2023-09-21 19:20:29 +0200

    summary

--- src/zajavka2/warsztat_01/ex30_summary/App.java
+++ src/zajavka2/warsztat_01/ex30_summary/App.java
@@ -1,7 +1,19 @@
 package zajavka2.warsztat_01.ex30_summary;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        List<String> stringList = new LinkedList<>();
+        stringList.add(null);
+        stringList.add(null);
+        stringList.add(null);
+
+
+        System.out.println(stringList);
     }
 }
