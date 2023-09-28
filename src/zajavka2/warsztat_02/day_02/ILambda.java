48e3e8d24f6a515d21d36c581362560ea9cf4fc5
Dariusz Okoński <darek200180@gmail.com>
2023-09-28 19:26:32 +0200

    lambda

---
+++ src/zajavka2/warsztat_02/day_02/ILambda.java
@@ -0,0 +1,6 @@
+package zajavka2.warsztat_02.day_02;
+
+public interface ILambda {
+    Integer countsChars(String input1, String input2);
+}
+
---
+++ src/zajavka2/warsztat_02/day_02/Main.java
@@ -0,0 +1,14 @@
+package zajavka2.warsztat_02.day_02;
+
+import java.util.function.Function;
+
+public class Main {
+    public static void main(String[] args) {
+        ILambda iLambda = (value, value2) -> {
+            return value.length() + value2.length();
+        };
+
+        System.out.println(iLambda.countsChars("Zajavka", "hello"));
+    }
+}
+
