410e83e23879881be45c02439a7e9ef52475b22c
Dariusz Okoński <darek200180@gmail.com>
2023-10-16 18:37:50 +0200

    day 13

--- src/zajavka2/warsztat_02/day_13/Main.java
+++ src/zajavka2/warsztat_02/day_13/Main.java
@@ -6,7 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Function<Integer, String> integerStringFunction = i -> {
             if(i > 0) {
-                throw  new MyException(i + "greater than 0!");
+                try {
+                    throw  new MyException(i + "greater than 0!");
+                } catch (MyException e) {
+                    throw new RuntimeException(e);
+                }
             } else {
                 return i.toString();
             }
--- src/zajavka2/warsztat_02/day_13/MyException.java
+++ src/zajavka2/warsztat_02/day_13/MyException.java
@@ -1,6 +1,6 @@
 package zajavka2.warsztat_02.day_13;
 
-public class MyException extends RuntimeException {
+public class MyException extends Exception {
     public MyException(String message) {
         super(message);
     }
