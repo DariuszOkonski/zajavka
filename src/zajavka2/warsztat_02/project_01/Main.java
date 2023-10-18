be47a0325e864dd61bad8327e9816676e2c48709
Dariusz Okoński <darek200180@gmail.com>
2023-10-18 19:46:35 +0200

    add Main

--- src/zajavka2/warsztat_02/project_01/Main.java
+++ src/zajavka2/warsztat_02/project_01/Main.java
@@ -1,5 +1,12 @@
 package zajavka2.warsztat_02.project_01;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
 public class Main {
     public static void main(String[] args) {
         MyConsumer myConsumer = new MyConsumer();
@@ -7,25 +14,25 @@ public class Main {
         myConsumer.consumeSupplier(() -> "123");
         myConsumer.consumeFunction(Object::toString);
 
-//        Function<String, Integer> myFunction = Transformer.getFunctionImpl();
-//        Optional<Integer> myOptional = Optional.of("myString").map(myFunction);
-//        System.out.println(myOptional);
-//
-//        UnaryOperator<BigDecimal> myUnaryOperator = Transformer.getUnaryOperatorImpl();
-//        Optional<BigDecimal> myResult = Optional.of(BigDecimal.valueOf(3.14)).map(myUnaryOperator);
-//        System.out.println(myResult);
+        Function<String, Integer> myFunction = Transformer.getFunctionImpl();
+        Optional<Integer> myOptional = Optional.of("myString").map(myFunction);
+        System.out.println(myOptional);
+
+        UnaryOperator<BigDecimal> myUnaryOperator = Transformer.getUnaryOperatorImpl();
+        Optional<BigDecimal> myResult = Optional.of(BigDecimal.valueOf(3.14)).map(myUnaryOperator);
+        System.out.println(myResult);
 
 
-//        Producer<String> myProducer = new Producer<>();
-//
-//        Supplier<String> mySuppliers = myProducer.getSupplierImpl("myAwesomeString");
-//        System.out.println(mySuppliers.get());
-//
-//        Consumer<String> myConsumer = myProducer.getConsumer();
-//        myConsumer.accept("Consumed");
-//
-//        Function<String, String> myFunction = myProducer.getFunction("b");
-//        System.out.println(myFunction.apply("a"));
+        Producer<String> myProducer = new Producer<>();
+
+        Supplier<String> mySuppliers = myProducer.getSupplierImpl("myAwesomeString");
+        System.out.println(mySuppliers.get());
+
+        Consumer<String> myConsumer2 = myProducer.getConsumer();
+        myConsumer2.accept("Consumed");
+
+        Function<String, String> myFunction2 = myProducer.getFunction("b");
+        System.out.println(myFunction2.apply("a"));
     }
 }
 
