a649078cd3a27fd8e7ccdb08042b4aaea1195c5a
Dariusz Okoński <darek200180@gmail.com>
2023-10-18 18:44:49 +0200

    project 1

--- src/zajavka2/warsztat_02/project_01/Main.java
+++ src/zajavka2/warsztat_02/project_01/Main.java
@@ -1,13 +1,17 @@
 package zajavka2.warsztat_02.project_01;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        // 11:00 min
         Producer<String> myProducer = new Producer<>();
+
         Supplier<String> mySuppliers = myProducer.getSupplierImpl("myAwesomeString");
         System.out.println(mySuppliers.get());
+
+        Consumer<String> myConsumer = myProducer.getConsumer();
+        myConsumer.accept("Consumed");
     }
 }
 
--- src/zajavka2/warsztat_02/project_01/Producer.java
+++ src/zajavka2/warsztat_02/project_01/Producer.java
@@ -1,9 +1,16 @@
 package zajavka2.warsztat_02.project_01;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Producer<T> {
     public Supplier<T> getSupplierImpl(T t) {
         return () -> t;
+    }
+
+    public Consumer<T> getConsumer() {
+        return u -> {
+            System.out.println("Consumer: " + u);
+        };
     }
 }
