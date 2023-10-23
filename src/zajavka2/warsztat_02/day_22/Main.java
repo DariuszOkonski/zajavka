d31cc3569f7ad73b1b1ef9263e1ee0eb6fd31718
Dariusz Okoński <darek200180@gmail.com>
2023-10-23 18:18:31 +0200

    day 22

---
+++ src/zajavka2/warsztat_02/day_22/Main.java
@@ -0,0 +1,11 @@
+package zajavka2.warsztat_02.day_22;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> stringList = List.of("v1", "v2", "v3");
+
+
+    }
+}
