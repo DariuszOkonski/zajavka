7464f7ec398291d6558cfefdd5e4233e43e60f88
Dariusz Okoński <darek200180@gmail.com>
2023-10-24 18:54:04 +0200

    day 23

--- src/zajavka2/warsztat_02/day_23/Main.java
+++ src/zajavka2/warsztat_02/day_23/Main.java
@@ -1,7 +1,20 @@
 package zajavka2.warsztat_02.day_23;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(8);
+
+        Stream<Integer> stream = integerList.stream();
+                stream.reduce(0, (currentState, nextValue) -> currentState + nextValue);
+                stream.reduce(0, (currentState, nextValue) -> currentState + nextValue);
 
     }
 }
