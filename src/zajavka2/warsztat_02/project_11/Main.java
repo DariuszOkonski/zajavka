db63e7f76e9d6e9a00bd978277bc61ebfc15843e
Dariusz Okoński <darek200180@gmail.com>
2023-11-07 17:42:45 +0100

    warsztat 3

---
+++ src/zajavka2/warsztat_02/project_11/Main.java
@@ -0,0 +1,7 @@
+package zajavka2.warsztat_02.project_11;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/zajavka2/warsztat_03/entry_01/Main.java
@@ -0,0 +1,7 @@
+package zajavka2.warsztat_03.entry_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("entry 1");
+    }
+}
